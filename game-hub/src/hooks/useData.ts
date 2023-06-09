4a69ebbae7f93c5881a596b0c07056e3af664c94
Raghav-Awasthi <awasthi31052004@gmail.com>
2023-06-09 13:01:52 +0530

    Fixed bugs in data fetching hook

--- game-hub/src/hooks/useData.ts
+++ game-hub/src/hooks/useData.ts
@@ -1,38 +1,37 @@
 import { CanceledError } from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-
 interface FetchResponse<T> {
-    count: number;
-    results: T[];
+  count: number;
+  results: T[];
 }
 
 const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
     const controller = new AbortController();
 
-    const [data, setData] = useState<T[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-  
-    useEffect(() => {
-      setLoading(true)
-      apiClient
-        .get<FetchResponse<T>>(endpoint, { signal: controller.signal})
-        .then((res) =>{ 
-          setData(res.data.results)
-          setLoading(false)
-        })
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-            setLoading(false)
-        });
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+        setLoading(false);
+      });
 
-        return () => controller.abort();
-    }, []);
+    return () => controller.abort();
+  }, []);
 
-    return { data, error, isLoading } 
-}
+  return { data, error, isLoading };
+};
 
-export default useData
+export default useData;
